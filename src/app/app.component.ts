11a0a1cdd2f93c9768a7cf9494631c659aa97ccf
Lautaro Avila <lavila@eprern.gov.ar>
2023-08-25 15:01:30 -0300

    Actualizado a ultimo momento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,55 +3,83 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProvinciasService } from './services/provincias/provincias.service';
 import { PersonasService } from './services/personas/personas.service';
 import { PaisesService } from './services/paises/paises.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  
+export class AppComponent implements OnInit {
+
   personaFom!: FormGroup;
-  paises : any;
+  paises: any;
   provincias: any;
+  personas: any;
 
   constructor(public fb: FormBuilder,
-              public provinciasService:ProvinciasService,
-              public personasService: PersonasService,
-              public paisesService:PaisesService){}
+    public provinciasService: ProvinciasService,
+    public personasService: PersonasService,
+    public paisesService: PaisesService) { }
 
   ngOnInit(): void {
     this.personaFom = this.fb.group({
-      nombre : ['',Validators.required],
-      apellido : ['',Validators.required],
-      edad : ['',Validators.required],
-      pais : ['',Validators.required],
-      provincia : ['',Validators.required],
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required],
+      pais: ['', Validators.required],
+      provincia: ['', Validators.required],
     });
 
     this.paisesService.getAllPaises().subscribe(resp => {
       this.paises = resp;
       // console.log(resp)
     },
-    error=>{console.error(error)})
-  
+      error => { console.error(error) });
+
+
+    this.personasService.getAllPersonas().subscribe(resp => {
+      this.personas = resp;
+    },
+      error => { console.error(error) }
+    );
+
     this.personaFom.get('pais')?.valueChanges.subscribe(value => {
       this.provinciasService.getAllProvinciasByPais(value.id).subscribe(resp => {
         this.provincias = resp;
       },
-      error => {console.error(error)});
+        error => { console.error(error) });
     })
-  
+
   }
 
-  guardar(){}
-
-  cargarProvinciasPorPaisesId(event:any){
-    this.provinciasService.getAllProvinciasByPais(event.target.value).subscribe(resp => {
-      this.provincias = resp;
+  guardar() {
+    Swal.fire({
+      title: 'Excelente!',
+      text: 'Persona Guardada Exitosamente!',
+      icon: 'success'
+    })
+    this.personasService.savePersona(this.personaFom.value).subscribe(resp => {
+      this.personaFom.reset();
+      this.personas.push(resp);
     },
-    error=>{console.error(error)})
+      error => { console.error(error) })
   }
 
-  
+
+  borrar(){
+    Swal.fire({
+      title:'Eliminar Registro',
+      text:'¿Estás seguro de eliminar el registro?',
+      icon:'warning',
+    })
+  }
+  // cargarProvinciasPorPaisesId(event: any) {
+  //   this.provinciasService.getAllProvinciasByPais(event.target.value).subscribe(resp => {
+  //     this.provincias = resp;
+  //   },
+  //     error => { console.error(error) })
+  // }
+
+
 }
--- src/app/services/personas/personas.service.ts
+++ src/app/services/personas/personas.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonasService {
+  private API_SERVER: string = "http://localhost:8080/persona/";
 
-  constructor() { }
+  constructor( private httpClient: HttpClient) { }
+
+
+  public getAllPersonas(){
+    return this.httpClient.get(this.API_SERVER);
+  }
+
+
+  public savePersona(persona:any): Observable<any>{
+      return this.httpClient.post(this.API_SERVER,persona)
+  }
 }
